adae717fcaf386056d00c549e89ae0aac6ef9f3d
billvivino <76108269+billvivino@users.noreply.github.com>
2025-01-20 06:27:39 -0500

    Initial Commit

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Bill Vivino on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
